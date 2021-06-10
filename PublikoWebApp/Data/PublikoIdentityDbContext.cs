8c5d4ff223b0ede8c600d7891bdc238a86073dc3
J.G <71701248+Julio-German-Gutierrez@users.noreply.github.com>
2021-06-10 12:57:28 +0200

    Add project files.

---
+++ PublikoWebApp/Data/PublikoIdentityDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PublikoWebApp.Data
+{
+    public class PublikoIdentityDbContext : IdentityDbContext
+    {
+        public PublikoIdentityDbContext(DbContextOptions<PublikoIdentityDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
