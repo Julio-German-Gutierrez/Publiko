53931592a66b0ee2cee474bd66a483ed2fc6bba2
J.G <71701248+Julio-German-Gutierrez@users.noreply.github.com>
2021-09-08 23:42:56 +0200

    test 8

--- PublikoAPI/Controllers/PagesController.cs
+++ PublikoAPI/Controllers/PagesController.cs
@@ -23,7 +23,7 @@ namespace PublikoAPI.Controllers
             _globalServices = globalServices;
         }
 
-        public PublikoPagesDBContext _pagesDBContext { get; } //test 7
+        public PublikoPagesDBContext _pagesDBContext { get; } //test 8
         public IGlobalIDServices _globalServices { get; }
 
 
